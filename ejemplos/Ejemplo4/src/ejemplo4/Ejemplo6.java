feda0dc1781fb22839186ec77b023fe67be13f19
svalverde007 <servio.valverde.uemv@gmail.com>
2025-11-13 11:35:57 -0500

    placas

--- ejemplos/Ejemplo4/src/ejemplo4/Ejemplo6.java
+++ ejemplos/Ejemplo4/src/ejemplo4/Ejemplo6.java
@@ -18,21 +18,22 @@ public class Ejemplo6 {
         // 
         Scanner entrada = new Scanner(System.in);
         entrada.useLocale(Locale.US);
-        System.out.println("Ingrese la ciudad");
-        String ciudad = entrada.nextLine();
+        System.out.println("Ingrese la placa");
+        String placa = entrada.nextLine();
 
         // L    0
         // O    1
         // J    2 
         // A    3
-        // 
-        String inicial = ciudad.substring(0, 1); // se obtiene 
+        
+        String inicial = placa.substring(0, 1); // se obtiene 
         // el primer 
         // caracter de la cadena
         // Si el valor de ciudad es 
         // "Loja" se obtiene "L"
 
-        if ( inicial.equals("L") || inicial.equals("l")) {
+        if ( inicial.equals("P") || inicial.equals("G") 
+                || inicial.equals("p") || inicial.equals("g")) {
             System.out.printf("%s\n", "acceso correcto");
         } else {
             System.out.printf("%s\n", "acceso incorrecto");
